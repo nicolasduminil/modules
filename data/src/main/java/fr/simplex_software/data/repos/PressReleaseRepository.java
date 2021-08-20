7a337eaea15d94f4d1880c7dbffa6142d56db43e
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2021-08-20 18:10:05 +0200

    Have removed @Repository

--- data/src/main/java/fr/simplex_software/data/repos/PressReleaseRepository.java
+++ data/src/main/java/fr/simplex_software/data/repos/PressReleaseRepository.java
@@ -3,7 +3,6 @@ package fr.simplex_software.data.repos;
 import fr.simplex_software.commons.*;
 import org.springframework.stereotype.*;
 
-@Repository
-public interface PressReleaseRepository extends CustomJpaRepository/*JpaRepository*/
+public interface PressReleaseRepository extends CustomJpaRepository
 {
 }
