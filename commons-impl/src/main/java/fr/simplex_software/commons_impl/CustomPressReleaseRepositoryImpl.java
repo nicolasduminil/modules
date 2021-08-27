f7ddfdb71e61a81a127a9c9939bf646856b2f2b6
Nicolas Duminil <nicolas.duminil@simplex-software.fr>
2021-08-27 15:52:03 +0200

    Have fixed exception PropertyReferenceException: No property findOne found for type PressReleaseEntity !

--- commons-impl/src/main/java/fr/simplex_software/commons_impl/CustomPressReleaseRepositoryImpl.java
+++ commons-impl/src/main/java/fr/simplex_software/commons_impl/CustomPressReleaseRepositoryImpl.java
@@ -1,9 +1,7 @@
-package fr.simplex_software.commons;
+package fr.simplex_software.commons_impl;
 
+import fr.simplex_software.commons.*;
 import fr.simplex_software.jpa.*;
-import org.springframework.data.domain.*;
-
-import java.util.*;
 
 public class CustomPressReleaseRepositoryImpl implements CustomPressReleaseRepository
 {
--- data/src/main/java/fr/simplex_software/data/repos/PressReleaseRepository.java
+++ data/src/main/java/fr/simplex_software/data/repos/PressReleaseRepository.java
@@ -1,9 +1,8 @@
 package fr.simplex_software.data.repos;
 
-import fr.simplex_software.commons.CustomPressReleaseRepository;
+import fr.simplex_software.commons.*;
 import fr.simplex_software.jpa.*;
 import org.springframework.data.jpa.repository.*;
-import org.springframework.stereotype.*;
 
 public interface PressReleaseRepository extends JpaRepository<PressReleaseEntity, Long>, CustomPressReleaseRepository
 {
